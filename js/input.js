df53da635917dc35b5fe38f5d39b9b409b2d0ed0
Londoño <londono@192.168.1.56>
2018-12-18 18:42:20 -0500

    first commit

--- js/input.js
+++ js/input.js
@@ -1,23 +1,46 @@
-function myFunction() {
-    let input, filter, ul, li, a, i, txtValue;
+var normalize = (function() {
+    var from = "ÃÀÁÄÂÈÉËÊÌÍÏÎÒÓÖÔÙÚÜÛãàáäâèéëêìíïîòóöôùúüûÑñÇç", 
+        to   = "AAAAAEEEEIIIIOOOOUUUUaaaaaeeeeiiiioooouuuunncc",
+        mapping = {};
+   
+    for(var i = 0, j = from.length; i < j; i++ )
+        mapping[ from.charAt( i ) ] = to.charAt( i );
+   
+    return function( str ) {
+        var ret = [];
+        for( var i = 0, j = str.length; i < j; i++ ) {
+            var c = str.charAt( i );
+            if( mapping.hasOwnProperty( str.charAt( i ) ) )
+                ret.push( mapping[ c ] );
+            else
+                ret.push( c );
+        }      
+        return ret.join( '' );
+    }
+   
+  })();
+
+function myFunction(){
+    let input, filter, section, h2, txtValue, div;
     input = document.getElementById('myInput');
     filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName('li');
+    section = document.getElementById("contain");
+    div = section.getElementsByTagName('div');
 
-    //recorrido en la busqueda
 
-    for( i= 0; i < li.length; i++){
-        a = li[i].getElementsByTagName("a") [0];
-        txtValue = a.textContent || a.innerText;
-         if (txtValue.toUpperCase().indexOf(filter) >= 0) {
-            li[i].style.display = "";
-            ul.style.display = "inline";
-         }else{
-            li[i].style.display = "none";
-        } if(input.value === ""){
-            ul.style.display = "none";
+    for( let i = 0; i < div.length; i++){
+        h2 = div[i].getElementsByTagName("h2") [0];
+        txtValue = normalize(h2.textContent) || normalize(h2.innerText);
+        if(txtValue.toUpperCase().indexOf(filter) > -1){
+        div[i].style.display = "";
+        }else {
+         div[i].style.display = "none";
+        }
+
+        if(filter === ""){
+            div[i].style.display = "";
         }
     }
-
+      
 }
+
